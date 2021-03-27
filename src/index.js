99d3af248632401035a105d72c6f6ee6651c2089
Marcus <marcus.daley1@outlook.com>
2021-03-27 13:53:16 -0400

    added function to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// function mOver(obj) {
-//     obj.innerHTML = "A.K.A DR.DAL3Y"
-//   }
+function mOver(obj) {
+    obj.innerHTML = "A.K.A DR.DAL3Y"
+  }
   
-//   function mOut(obj) {
-//     obj.innerHTML = "MARCUS DALEY"
-// }
+  function mOut(obj) {
+    obj.innerHTML = "MARCUS DALEY"
+}
